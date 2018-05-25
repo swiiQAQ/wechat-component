62689eb592a67a69d9bdb9c67c7d09d7f3ce6cf0
罗梦婕 <luomj@metersbonwe.com>
2018-05-25 17:56:23 +0800

    滑动删除

--- pages/components/pages/slideDel/slideDel.js
+++ pages/components/pages/slideDel/slideDel.js
@@ -7,22 +7,64 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    delWidth: String,
+    delText: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    //触摸起始坐标
+    startX: '',
+    moveDistance: '',
+    transitionStyle: '',
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    touchStartHandler: function(e){
+      this.setData({transitionStyle: ''});
+      this.setData({startX: e.touches[0].clientX});
+    },
     touchMoveHandler:function(e){
-      console.log(e);
+      var index = e.currentTarget.dataset.index;
+      var clientX = e.touches[0].clientX;
+      var distance = clientX-this.data.startX;
+      var moveDistance = this.data.moveDistance;
+      var delWidth = Number(this.properties.delWidth);
+
+      if(distance + delWidth <0){
+        distance = -delWidth;
+      }else if(distance>0){
+        distance = 0;
+        this.setData({
+          transitionStyle: 'left 0.2s ease-in-out'
+        })
+      }
+      this.setData({
+        moveDistance: distance,
+        moveIndex: index
+      });
+    },
+    touchEndHandler:function(e){
+      var end = e.changedTouches[0].clientX;
+      var start = this.data.startX;
+      var delWidth = Number(this.properties.delWidth);
+      if(start-end>delWidth/2){
+        this.setData({ 
+          moveDistance : -delWidth,
+          transitionStyle: 'left 0.2s ease-in-out'
+        });
+      }
+      else{
+        this.setData({ 
+          moveDistance : 0,
+          transitionStyle: 'left 0.2s ease-in-out'
+        });
+      }
     }
   }
 })
